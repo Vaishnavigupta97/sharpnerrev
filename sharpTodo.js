aa71607be5ad6d06441ab8c87476eab1cf980c05
Vaishnavigupta97 <vaishnavigupta6389@gmail.com>
2023-10-13 11:52:39 +0530

    first commit

---
+++ sharpTodo.js
@@ -0,0 +1,6 @@
+var header_title = document.getElementById("header-title");
+header_title.style.border = "1px solid black";
+
+var title = document.getElementsByClassName("title");
+title[0].style.color = "green"
+
